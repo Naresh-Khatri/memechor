3e69dab2ecaa31ad5e2b565a1748adbf9b340d62
Naresh <naresh.khatri2345@gmail.com>
2022-02-22 18:56:41 +0530

    Install and use node-schedule instead of cron

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 import { IgApiClient } from "instagram-private-api";
+import { scheduleJob } from "node-schedule";
 import "dotenv/config";
 
-import chor from './memeChor.js'
+import chor from "./memeChor.js";
 import getImgBuffer from "./getImgBuffer.js";
 
-(async () => {
-  const ig = new IgApiClient();
-  ig.state.generateDevice(process.env.IG_USERNAME);
+async function main() {
+  try {
+    const ig = new IgApiClient();
+    ig.state.generateDevice(process.env.IG_USERNAME);
 
-  const loggedInUser = await ig.account.login(
-    process.env.IG_USERNAME,
-    process.env.IG_PASSWORD
-  );
-  console.log("login successful!");
+    const loggedInUser = await ig.account.login(
+      process.env.IG_USERNAME,
+      process.env.IG_PASSWORD
+    );
+    console.log("login successful!");
 
-  //call chor to get a random post
-  const post = await chor();
-  console.log('post: ', post);
+    //call chor to get a random post
+    const post = await chor();
+    console.log("post: ", post);
 
-  //public chori ka meme
-  const publishResult = await ig.publish.photo({
-    file: await getImgBuffer(post.photoURL),
-    caption: post.caption,
-  });
-  console.log(publishResult)
-})();
+    //public chori ka meme
+    const publishResult = await ig.publish.photo({
+      file: await getImgBuffer(post.photoURL),
+      caption: post.caption,
+    });
+    console.log(publishResult);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+scheduleJob("* * * * *", main);
