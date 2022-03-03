2ea79f78cf9d4b0c4d387e5f1b9937c60197e6ed
Naresh <naresh.khatri2345@gmail.com>
2022-03-03 23:23:55 +0530

    catch and reject when the image is removed from in the photoURL

--- getImgBuffer.js
+++ getImgBuffer.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 export default function getImgBuffer(URL) {
-  return new Promise((resolve, reject) => {
-    axios.get(URL, { responseType: "arraybuffer" }).then((res) => {
-      const imgbuffer = Buffer.from(res.data, "binary");
+  return new Promise(async (resolve, reject) => {
+    try{
+      const resImg = await axios.get(URL, { responseType: "arraybuffer" });
+      const imgbuffer = Buffer.from(resImg.data, "binary");
       resolve(imgbuffer);
-    });
+    }
+    catch(err){
+      reject({message:"Image probably removed"});
+    }
   });
 }
