ecfcbe7dd9df0feafc8e2fa0379f98bb8b1a9492
Naresh <naresh.khatri2345@gmail.com>
2022-03-06 22:17:40 +0530

    Filter out nsfw memes and put subreddit name in env file

--- getDailyMemes.js
+++ getDailyMemes.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import fs from "fs";
+import 'dotenv/config'
 
 export default async function getDailyMemes() {
+  const subreddit = process.env.SUBREDDIT;
   const URLs = [
-    "https://www.reddit.com/r/ProgrammerHumor/hot.json",
-    "https://www.reddit.com/r/ProgrammerHumor/top.json",
-    "https://www.reddit.com/r/ProgrammerHumor/new.json",
-    "https://www.reddit.com/r/ProgrammerHumor/rising.json",
+    "https://www.reddit.com/r/" + subreddit + "/hot.json",
+    "https://www.reddit.com/r/" + subreddit + "/top.json",
+    "https://www.reddit.com/r/" + subreddit + "/new.json",
+    "https://www.reddit.com/r/" + subreddit + "/rising.json",
   ];
   const promises = URLs.map((url) => axios.get(url));
   const results = await Promise.allSettled(promises);
@@ -16,7 +18,7 @@ export default async function getDailyMemes() {
   results.forEach((result) => {
     const posts = result.value.data.data.children;
     if (!posts) return;
-    const filteredPosts = getFilteredPostsWithJpg(posts);
+    const filteredPosts = getFilteredPosts(posts);
     filteredPosts.forEach((post) => {
       //dont want to add duplicates
       if (uniquePosts.includes(post)) return;
@@ -42,10 +44,10 @@ export default async function getDailyMemes() {
   let prevPosts = await fs.promises.readFile("postsCollection.json", "utf8");
   prevPosts = JSON.parse(prevPosts);
   uniquePosts.forEach((uniquePost) => {
-    if (prevPosts.find((prevPost) => prevPost.id == uniquePost.id)){
+    if (prevPosts.find((prevPost) => prevPost.id == uniquePost.id)) {
       // console.log(uniquePost.photoURL)
       return;
-    } 
+    }
     prevPosts.push(uniquePost);
   });
   //write to file
@@ -57,6 +59,13 @@ export default async function getDailyMemes() {
   });
 }
 
-const getFilteredPostsWithJpg = (posts) => {
-  return posts.filter((post) => post.data.url.slice(-4) == ".jpg");
+const getFilteredPosts = (posts) => {
+  return posts.filter(
+    (post) =>
+      //filter out non-jpg-image posts
+      post.data.url.slice(-4) == ".jpg" &&
+      //filter out nsfw posts
+      post.data.whitelist_status == "all_ads"
+  );
 };
+getDailyMemes();
